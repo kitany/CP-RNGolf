284cc576a313f1be39b70efec25d2cf720597057
Kayla Nguyen <kthuyt08@gmail.com>
2025-01-29 21:39:51 -0800

    Add files via upload

--- src/Play.js
+++ src/Play.js
@@ -9,6 +9,9 @@ class Play extends Phaser.Scene {
     this.SHOT_VELOCITY_Y_MIN = 700
     this.SHOT_VELOCITY_Y_MAX = 1100
 
+    // challenge
+    this.shotCounter = 0
+    this.score = 0
   }
 
   preload() {
@@ -25,7 +28,7 @@ class Play extends Phaser.Scene {
     this.grass = this.add.image(0, 0, 'grass').setOrigin(0)
 
     // add cup
-    this.cup = this.physics.add.sprite(width / 2, height / 10, 'cup') // height / 10  height - height / 5
+    this.cup = this.physics.add.sprite(width / 2, height / 10, 'cup')
     this.cup.body.setCircle(this.cup.width / 4)
     this.cup.body.setOffset(this.cup.width / 4)
     this.cup.body.setImmovable(true)
@@ -38,15 +41,16 @@ class Play extends Phaser.Scene {
     this.ball.body.setDamping(true).setDrag(0.5)
 
     // add walls
-    let wallA = this.physics.add.sprite(0, height / 4, 'wall')
-    wallA.setX(Phaser.Math.Between(0 + wallA.width / 2, width - wallA.width / 2))
-    wallA.body.setImmovable(true)
+    this.wallA = this.physics.add.sprite(0, height / 4, 'wall')
+    this.wallA.setX(Phaser.Math.Between(0 + this.wallA.width / 2, width - this.wallA.width / 2))
+    this.wallADirection = 1;
+    this.wallA.body.setImmovable(true)
 
-    let wallB = this.physics.add.sprite(0, height / 2, 'wall') // ctrl+D select multiple instances
-    wallB.setX(Phaser.Math.Between(0 + wallB.width / 2, width - wallB.width / 2))
-    wallB.body.setImmovable(true)
+    this.wallB = this.physics.add.sprite(0, height / 2, 'wall') // ctrl+D select multiple instances
+    this.wallB.setX(Phaser.Math.Between(0 + this.wallB.width / 2, width - this.wallB.width / 2))
+    this.wallB.body.setImmovable(true)
 
-    this.walls = this.add.group([wallA, wallB])
+    this.walls = this.add.group([this.wallA, this.wallB])
 
     // add one-way
     this.oneWay = this.physics.add.sprite(0, height / 4 * 3, 'oneway')
@@ -56,14 +60,21 @@ class Play extends Phaser.Scene {
 
     // add pointer input
     this.input.on('pointerdown', (pointer) => {
-      let shotDirection = pointer.y <= this.ball.y ? 1 : -1 // pointer is above ball
-      this.ball.body.setVelocityX(Phaser.Math.Between(-this.SHOT_VELOCITY_X, this.SHOT_VELOCITY_X))
-      this.ball.body.setVelocityY(Phaser.Math.Between(this.SHOT_VELOCITY_Y_MIN, this.SHOT_VELOCITY_Y_MAX) * shotDirection)
+      this.shotCounter += 1 // increase shot counter
+      let shotDirectionY = pointer.y <= this.ball.y ? 1 : -1 // pointer is above ball
+      let shotDirectionX = pointer.x <= this.ball.x ? 1 : -1; // add x direction
+
+      this.ball.body.setVelocityX(Phaser.Math.Between(1, this.SHOT_VELOCITY_X) * shotDirectionX)
+      this.ball.body.setVelocityY(Phaser.Math.Between(this.SHOT_VELOCITY_Y_MIN, this.SHOT_VELOCITY_Y_MAX) * shotDirectionY)
     })
 
     // cup/ball collision
     this.physics.add.collider(this.ball, this.cup, (ball, cup) => {
-      ball.destroy() // disable body and delete it
+      // ball.destroy() // disable body and delete it
+      this.score += 1
+      ball.setVelocity(0)
+      ball.x = width / 2;
+      ball.y = height - height / 10;
     })
 
     // ball/wall collision
@@ -71,17 +82,30 @@ class Play extends Phaser.Scene {
 
     // ball/one-way collision
     this.physics.add.collider(this.ball, this.oneWay)
+
+    // shot counter and percentage
+    this.shotText = this.add.text(20, 20, `Shots: ${this.shotCounter}`, { fontFamily: 'Arial', fontSize: '30px', color: '#000' });
+    this.successText = this.add.text(20, 50, `Success: 0%`, { fontFamily: 'Arial', fontSize: '30px', color: '#000' });
   }
 
   update() {
+    // moving obstacle
+    this.wallA.x += 5 * this.wallADirection;
+    if (this.wallA.x <= this.wallA.width / 2 || this.wallA.x >= width - this.wallA.width / 2) {
+      this.wallADirection *= -1;
+    }
 
+    // update shot counter and success percentage text
+    this.shotText.setText(`Shots: ${this.shotCounter}`);
+    let successPercentage = (this.shotCounter > 0) ? Math.floor((this.score / this.shotCounter) * 100) : 0;
+    this.successText.setText(`Success: ${successPercentage}%`);
   }
 }
 /*
 CODE CHALLENGE
 Try to implement at least 3/4 of the following features during the remainder of class (hint: each takes roughly 15 or fewer lines of code to implement):
-[ ] Add ball reset logic on successful shot
-[ ] Improve shot logic by making pointer’s relative x-position shoot the ball in correct x-direction
-[ ] Make one obstacle move left/right and bounce against screen edges
-[ ] Create and display shot counter, score, and successful shot percentage
+[+] Add ball reset logic on successful shot
+[+] Improve shot logic by making pointer’s relative x-position shoot the ball in correct x-direction
+[+] Make one obstacle move left/right and bounce against screen edges
+[+] Create and display shot counter, score, and successful shot percentage
 */
